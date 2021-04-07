118fbd4daf8246429d6d2b4529f4ae3255a0003b
salahgamal <81882072+salahgamal@users.noreply.github.com>
2021-04-07 05:04:59 +0200

    second task
    second task

---
+++ bin/second.dart
@@ -0,0 +1,5 @@
+import 'package:second/second.dart' as second;
+
+void main(List<String> arguments) {
+  print('Hello world: ${second.calculate()}!');
+}
---
+++ test/second_test.dart
@@ -0,0 +1,19 @@
+
+
+void main() {
+// حساب التقدير تبعا لدرجات طالب
+var mark=80;
+if (mark>=50 && mark<65){
+  print("passed");}
+
+  else if (mark>=65 && mark<75){
+    print("good");}
+  else if(mark>=75&& mark<85){
+    print("verygood");}
+  else if (mark>=85&& mark<=100 ) {
+  print("excellent");}
+
+
+
+  }
+
